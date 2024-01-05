5233c072f49439bd4bd25fafc25a06ab11c890a7
yoyozbi <yohan@yohanzbinden.ch>
2024-01-05 10:29:47 +0100

    fix errors in none-ls and lspconfig

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,19 +1,20 @@
 return {
-	
-	"nvimtools/none-ls.nvim",
-	config = function()
-		local null_ls = require("null-ls")
-		null_ls.setup({
-			sources = {
-				null_ls.builtins.formatting.stylua,
-				null_ls.builtins.diagnostics.eslint,
-				null_ls.builtins.code_actions.eslint_d
-				null_ls.builtins.completion.spell
-			}
-		})
-	end,
+ {	
+		"nvimtools/none-ls.nvim",
+		config = function()
+			local null_ls = require("null-ls")
+			null_ls.setup({
+				sources = {
+					null_ls.builtins.formatting.stylua,
+					null_ls.builtins.diagnostics.eslint,
+					null_ls.builtins.code_actions.eslint_d,
+					null_ls.builtins.completion.spell
+				}
+			})
+		end,
 
-	dependencies = {
-		"nvim-lua/plenary.nvim"
+		dependencies = {
+			"nvim-lua/plenary.nvim"
+		}
 	}
 }
