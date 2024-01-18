0f2e1dffbb629dd3b8bd4351742a0c321b629a6a
yoyozbi <yohan@yohanzbinden.ch>
2024-01-18 12:47:28 +0100

    add none-ls

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -8,7 +8,8 @@ return {
 					null_ls.builtins.formatting.stylua,
 					null_ls.builtins.diagnostics.eslint,
 					null_ls.builtins.code_actions.eslint_d,
-					null_ls.builtins.completion.spell
+					null_ls.builtins.completion.spell,
+					null_ls.builtins.diagnostics.ruff
 				}
 			})
 		end,
