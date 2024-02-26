519c33990269e849d228cfd7d0390eb82f11f9d6
yoyozbi <yohan@yohanzbinden.ch>
2024-02-26 10:07:39 +0100

    add treesitter svelte parser

--- lua/plugins/lspsaga.lua
+++ lua/plugins/lspsaga.lua
@@ -5,7 +5,7 @@ return {
 		config = function()
 			require('lspsaga').setup({})
 			require('nvim-treesitter.configs').setup({
-				ensure_installed = { "markdown", "markdown_inline", "regex", "bash" }
+				ensure_installed = { "markdown", "markdown_inline", "regex", "bash", 'svelte', 'typescript', 'javascript' }
 			})
 		end,
 		dependencies = {
